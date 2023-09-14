3b95dc4542e78be1e82fa328adcdfe2be46132af
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2023-09-14 17:41:42 -0400

    Added events search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,13 @@
 import EventList from '../../components/events/event-list';
 import { getAllEvents } from '../../dummy-data';
+import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
 	const events = getAllEvents();
 
 	return (
 		<div>
+    <EventsSearch  />
 			<EventList items={events} />
 		</div>
 	);
