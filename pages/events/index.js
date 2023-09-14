95377e09d2f656945f6fd563281e56841ebb611b
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2023-09-14 18:10:18 -0400

    Added routing to events search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,15 +1,22 @@
-import EventList from '../../components/events/event-list';
+import { Fragment } from 'react';
 import { getAllEvents } from '../../dummy-data';
+import { useRouter } from 'next/router';
+import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
 	const events = getAllEvents();
+	const router = useRouter();
+	function findEvents(year, month) {
+		const fullPath = `/events/${year}/${month}`;
 
+		router.push(fullPath);
+	}
 	return (
-		<div>
-    <EventsSearch  />
+		<Fragment>
+			<EventsSearch onSearch={findEvents} />
 			<EventList items={events} />
-		</div>
+		</Fragment>
 	);
 }
 
