7bba9d627d97f8cb9caae3b7a633dc2aaafc14ee
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2024-01-18 18:42:30 -0500

    Added getFilteredEvents functionality

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,34 +1,27 @@
 import { useRouter } from 'next/router';
-import { getFilteredEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import { getAllEvents } from '../../helpers/api-util';
 import { Fragment } from 'react';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/UI/button';
 import ErrorAlert from '../../components/UI/error-alert';
 
-function FilteredEventPage() {
+function FilteredEventPage(props) {
 	const router = useRouter();
 
-	const filterData = router.query.slug;
+	// const filterData = router.query.slug;
 
-	if (!filterData) {
-		return <p className='center'>Loading...</p>;
-	}
+	// if (!filterData) {
+	// 	return <p className='center'>Loading...</p>;
+	// }
 
-	const filteredYear = filterData[0];
-	const filteredMonth = filterData[1];
+	// const filteredYear = filterData[0];
+	// const filteredMonth = filterData[1];
 
-	const numYear = +filteredYear;
-	const numMonth = +filteredMonth;
+	// const numYear = +filteredYear;
+	// const numMonth = +filteredMonth;
 
-	if (
-		isNaN(numYear) ||
-		isNaN(numMonth) ||
-		numYear > 2030 ||
-		numYear < 2020 ||
-		numMonth > 12 ||
-		numMonth < 1
-	) {
+	if (props.hasError) {
 		return (
 			<Fragment>
 				<ErrorAlert>
@@ -41,10 +34,7 @@ function FilteredEventPage() {
 		);
 	}
 
-	const filteredEvents = getFilteredEvents({
-		year: numYear,
-		month: numMonth,
-	});
+	const filteredEvents = props.events;
 
 	if (!filteredEvents || filteredEvents.length === 0) {
 		return (
@@ -59,13 +49,69 @@ function FilteredEventPage() {
 		);
 	}
 
-	const date = new Date(numYear, numMonth - 1);
+	const date = new Date(props.date.year, props.date.month - 1);
+
 	return (
 		<Fragment>
 			<ResultsTitle date={date} />
 			<EventList items={filteredEvents} />
 		</Fragment>
 	);
+}
+
+export async function getFilteredEvents(dateFilter) {
+	const { year, month } = dateFilter;
+
+	const allEvents = await getAllEvents();
+
+	let filteredEvents = allEvents.filter((event) => {
+		const eventDate = new Date(event.date);
+		return (
+			eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+		);
+	});
+
+	return filteredEvents;
+}
+
+export async function getServerSideProps(context) {
+	const { params } = context;
+
+	const filterData = params.slug;
+
+	const filteredYear = filterData[0];
+	const filteredMonth = filterData[1];
+	const numYear = +filteredYear;
+	const numMonth = +filteredMonth;
+
+	if (
+		isNaN(numYear) ||
+		isNaN(numMonth) ||
+		numYear > 2030 ||
+		numYear < 2020 ||
+		numMonth > 12 ||
+		numMonth < 1
+	) {
+		return {
+			props: { hasError: true },
+		};
+	}
+
+	const filteredEvents = await getFilteredEvents({
+		year: numYear,
+		month: numMonth,
+	});	
+
+	return {
+		props: {
+			events: filteredEvents,
+			date: {
+				year: numYear,
+				month: numMonth,
+			},
+		}
+	}
+
 }
 
 export default FilteredEventPage;
